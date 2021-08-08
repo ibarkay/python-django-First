b06ee2a22d90866ce1e739997bf08b0d1e2c2479
ibarkay <ibarkay@gmail.com>
2021-08-08 11:13:59 +0300

    render html temps

--- pages/views.py
+++ pages/views.py
@@ -6,17 +6,18 @@ from django.shortcuts import render
 
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
-    print(request.user) 
-    return HttpResponse("<h1>Hello World</h1>")
+    print(request.user)
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse('<h2>Contact</h2>')
+    return render(request, "contact.html", {})
 
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse('<h2>About page</h2>')
+    return render(request, "about.html", {})
 
 
 def social_view(request, *args, **kwargs):
-    return HttpResponse('<h2>Social page</h2>')
+    return render(request, "social.html", {})
